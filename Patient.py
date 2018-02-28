850338328ae22cf8933652fec07597c624d0a401
aattas <aziz.attas@gmail.com>
2018-02-27 22:03:19 -0500

    Patient update 1

--- Patient.py
+++ Patient.py
@@ -8,5 +8,14 @@ class Patient:
         self.age = age
         self.hui = hui
         
-        
-        
+    """
+    Method to update paitent diabetic complication
+    Complications include:
+        1. Blindness, HUI = -0.170, weight = 0.552
+        2. Amputation, HUI = -0.105, weight = 0.102
+        3. Neuropathy, HUI = -0.065, weight = 0.072
+        4. Stroke,     HUI = -0.044, weight = 0.920
+        5. 
+    """
+    def complication(self):
+        pass
