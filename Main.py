8bf2b773b42d88fd03cb58a7d42ba5b2864d5fe7
aattas <aziz.attas@gmail.com>
2018-02-27 21:51:50 -0500

    update main 2

--- Main.py
+++ Main.py
@@ -13,12 +13,34 @@ def Sim(num_patients):
         """
         Generating a new age, hui, and hba1c for every loop.
         """
-        age = np.random.randint(18, 60) # random integer between 18 and 60
+        age = np.random.randint(18, 40) # random integer between 18 and 60
         hui = round(np.random.uniform(.7 , .9), 2) #random health utility index
         hba1c = round(np.random.uniform(6.5,14), 2) #random hba1c %
         patient = p.Patient(age,hui,hba1c) #patient instance
+        
+        """
+        1 quarter = 3months. 40 quarters = 10 years
+        choose quarter because HbA1c levels change every 3 months
+        """
+        quarter = 0
+        print("starting age= " + str(patient.age))
+        while patient.hui > 0 and quarter < 40:  
+            q_hba1c = patient.hba1c + round(np.random.uniform(-0.5,1), 2) # random change in Hba1C 
+            if q_hba1c - patient.hba1c == 1: # 1% change during quarter visit
+                patient.hui+= -0.3
+                print("decreased hui")
+            if quarter % 4 == 0: #year has passed
+                patient.age+= 1
+            quarter+=1
+            patient.hba1c = q_hba1c
+            
+        print("patient age =" + str(patient.age))
+        print("patient hui =" + str(patient.hui))
+        print("patient hba1c =" + str(patient.hba1c))
+        
+            
             
             
             
 
-Simulation = Sim(5) #call sim function
+Simulation = Sim(1) #call sim function
