0cbb5918d692e658c5238aee22ed2eb97b3fcea1
aattas <aziz.attas@gmail.com>
2018-03-01 07:07:04 -0500

    update 1 3/1

--- Main.py
+++ Main.py
@@ -24,23 +24,22 @@ def Sim(num_patients):
         """
         quarter = 0
         print("starting age= " + str(patient.age))
-        while patient.hui > 0 and quarter < 40:  
-            q_hba1c = patient.hba1c + round(np.random.uniform(-0.5,1), 2) # random change in Hba1C 
-            if q_hba1c - patient.hba1c == 1: # 1% change during quarter visit
-                patient.hui+= -0.3
+        while patient.hui > 0 and quarter < 120:  
+            q_hba1c = patient.hba1c + round(np.random.uniform(-0.5,1.5), 2) # random change in Hba1C 
+            # 1% change during quarter visit and HbA1C increased
+            if 0.9 <= abs(q_hba1c - patient.hba1c) <= 1.1 and q_hba1c - patient.hba1c > 0 : 
+                patient.hui+= -0.03
                 print("decreased hui")
+#            elif patient.hui < 1:
+#                patient.hui+= 0.03
             if quarter % 4 == 0: #year has passed
                 patient.age+= 1
             quarter+=1
-            patient.hba1c = q_hba1c
+            if 6.5 < patient.hba1c < 20: #capping hba1c at 20
+                patient.hba1c = q_hba1c
             
         print("patient age =" + str(patient.age))
         print("patient hui =" + str(patient.hui))
         print("patient hba1c =" + str(patient.hba1c))
         
-            
-            
-            
-            
-
 Simulation = Sim(1) #call sim function
--- Patient.py
+++ Patient.py
@@ -9,6 +9,7 @@ class Patient:
         self.hui = hui
         
     """
+    Method should take in self.hba1c as input and output the complication
     Method to update paitent diabetic complication
     Complications include:
         1. Blindness, HUI = -0.170, weight = 0.552
@@ -16,6 +17,18 @@ class Patient:
         3. Neuropathy, HUI = -0.065, weight = 0.072
         4. Stroke,     HUI = -0.044, weight = 0.920
         5. 
+    Complications will be determined by chance? and complication chance changes based on
+    the yearly HbA1C level.
     """
     def complication(self):
         pass
+    
+    """
+    Two methods to update patients attributes, quarterly update and yearly update
+    - Quarterly update will update HbA1C, and HUI given that there was a ~ 1%change
+    - Yearly Update will update HUI given that the patient developed a new complication
+        - Increment patient age +1
+        - Complications will be based on probability
+        - Each complication decreases HUI, this will occur every year from the time he developed that complication
+         until the patient dies or simulation reaches 100 years.
+    """
